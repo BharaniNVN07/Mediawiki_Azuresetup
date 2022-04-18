51032a4b2bea30266ebc7588b069b248a58b5715
NagaBharani <102315727+NagaBharani@users.noreply.github.com>
2022-04-18 20:12:29 +0530

    Update MariaDB.sh

--- MariaDB.sh
+++ MariaDB.sh
@@ -4,5 +4,5 @@ newUser='testuser'
 newDbPassword='testpwd'
 newDb='MediawikiDB'
 host=localhost
-CREATE DATABASE \`${newDb}\`;CREATE USER '${newUser}'@'${host}' IDENTIFIED BY '${newDbPassword}';GRANT USAGE ON *.* TO '${newUser}'@'${host}';GRANT ALL ON \`${newDb}\`.* TO '${newUser}'@'${host}';FLUSH PRIVILEGES;"
+commands="CREATE DATABASE \`${newDb}\`;CREATE USER '${newUser}'@'${host}' IDENTIFIED BY '${newDbPassword}';GRANT USAGE ON *.* TO '${newUser}'@'${host}';GRANT ALL ON \`${newDb}\`.* TO '${newUser}'@'${host}';FLUSH PRIVILEGES;"
 echo "${commands}" | /usr/bin/mysql -u root -p
