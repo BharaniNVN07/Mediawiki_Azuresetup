c25d297db5aabde1279b331834c27b3c8f9241cb
NagaBharani <102315727+NagaBharani@users.noreply.github.com>
2022-04-18 20:07:00 +0530

    Update Mediawiki.sh

--- Mediawiki.sh
+++ Mediawiki.sh
@@ -6,9 +6,9 @@ sudo tar -zxvf mediawiki.tar.gz
 sudo mkdir -p /var/www/mediawiki
 sudo mv mediawiki*/* /var/www/mediawiki
 
-chown -R www-data:www-data /var/www/mediawiki
+sudo chown -R www-data:www-data /var/www/mediawiki
 
 # Configure Mediawiwki - If this gives a 404 error then working a symbolic link should solve it
 sudo ln -s /var/www/mediawiki /var/www/html/mediawiki
 
-systemctl restart nginx
+sudo systemctl restart nginx
